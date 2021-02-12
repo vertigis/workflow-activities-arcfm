21d3fc5fb534a1cac8c8d443ab26d79469f32b8b
Ryan Cooney <rcooney@latitudegeo.com>
2021-02-12 10:51:19 -0800

    Add RunArcFMElectricTrace input documentation. Run prettier.

--- src/activities/RunArcFMElectricTrace.ts
+++ src/activities/RunArcFMElectricTrace.ts
@@ -1,83 +1,122 @@
 import type {
-  IActivityContext,
-  IActivityHandler,
+    IActivityContext,
+    IActivityHandler,
 } from "@geocortex/workflow/runtime/IActivityHandler";
 import { ChannelProvider } from "@geocortex/workflow/runtime/activities/core/ChannelProvider";
 import { activate } from "@geocortex/workflow/runtime/Hooks";
 
 /** An interface that defines the inputs of the activity. */
 export interface RunArcFMElectricTraceInputs {
-  /**
-   * @displayName Service URL
-   * @description The URL to the ArcGIS REST service.
-   * @required
-   */
-  serviceUrl: string;
+    /**
+     * @displayName Service URL
+     * @description The URL to the ArcGIS REST service. For example, http://server/arcgis/rest/services/<serviceName>/MapServer. The service must support the ArcFMMapServer extension.
+     * @required
+     */
+    serviceUrl: string;
 
-  /**
-   * @description The start point of the trace.
-   * @required
-   */
-  startPoint?: {
-    x: number;
-    y: number;
-    spatialReference?: {
-      wkid?: number;
-      wkt?: string;
+    /**
+     * @description The start point of the trace.
+     * @required
+     */
+    startPoint?: {
+        x: number;
+        y: number;
+        spatialReference?: {
+            wkid?: number;
+            wkt?: string;
+        };
     };
-  };
 
-  /**
-   * @description The type of trace to perform.
-   */
-  traceType?:
-    | "Downstream"
-    | "Upstream"
-    | "Distribution"
-    | "DownstreamProtective"
-    | "UpstreamProtective"
-    | "NextUpstreamProtective"
-    | "ProtectiveIsolation"
-    | string;
+    /**
+     * @description The type of trace to perform.
+     */
+    traceType?:
+        | "Downstream"
+        | "Upstream"
+        | "Distribution"
+        | "DownstreamProtective"
+        | "UpstreamProtective"
+        | "NextUpstreamProtective"
+        | "ProtectiveIsolation"
+        | string;
 
-  protectiveDevices?: string | number;
-  phasesToTrace?:
-    | "Any"
-    | "A"
-    | "B"
-    | "C"
-    | "AB"
-    | "AC"
-    | "BC"
-    | "ABC"
-    | "AtLeastA"
-    | "AtLeastB"
-    | "AtLeastC"
-    | "AtLeastAB"
-    | "AtLeastAC"
-    | "AtLeastBC"
-    | "AnySinglePhase"
-    | "AnyTwoPhases"
-    | string;
-  drawComplexEdges?: boolean;
-  includeEdges?: boolean;
-  includeJunctions?: boolean;
-  returnAttributes?: boolean;
-  returnGeometries?: boolean;
-  tolerance?: number;
-  spatialReference?: {
-    wkid?: number;
-    wkt?: string;
-  };
-  currentStatusProgID?: any;
+    /**
+     * @description The layer IDs corresponding to the protective devices that participate in the trace.
+     */
+    protectiveDevices?: string | number;
+
+    /**
+     * @description The phases to be included in the trace.
+     */
+    phasesToTrace?:
+        | "Any"
+        | "A"
+        | "B"
+        | "C"
+        | "AB"
+        | "AC"
+        | "BC"
+        | "ABC"
+        | "AtLeastA"
+        | "AtLeastB"
+        | "AtLeastC"
+        | "AtLeastAB"
+        | "AtLeastAC"
+        | "AtLeastBC"
+        | "AnySinglePhase"
+        | "AnyTwoPhases"
+        | string;
+
+    /**
+     * @description Whether the trace should draw complex edges. The default is false.
+     */
+    drawComplexEdges?: boolean;
+
+    /**
+     * @description Whether the user has identified edges to be included in the trace. The default is true.
+     */
+    includeEdges?: boolean;
+
+    /**
+     * @description Whether the user has identified junctions  to be included in the trace. The default is true.
+     */
+    includeJunctions?: boolean;
+
+    /**
+     * @description Whether to include attribute values in the trace results. The default is false.
+     */
+    returnAttributes?: boolean;
+
+    /**
+     * @description Whether to include feature geometries in the trace results. The default is true.
+     */
+    returnGeometries?: boolean;
+
+    /**
+     * @description A numerical value indicating the search tolerance for the trace in map units.
+     */
+    tolerance?: number;
+
+    /**
+     * @description The spatial reference for the results. By default, the results use the spatial reference of the map service.
+     */
+    spatialReference?: {
+        wkid?: number;
+        wkt?: string;
+    };
+
+    /**
+     * @description The progID of the current status object.
+     */
+    currentStatusProgID?: any;
 }
 
 /** An interface that defines the outputs of the activity. */
 export interface RunArcFMElectricTraceOutputs {
-  /**
-   * @description The result of the activity.
-   */
-  results: any[];
+    /**
+     * @description The result of the activity.
+     */
+    results: any[];
 }
 
 /**
@@ -87,41 +126,41 @@ export interface RunArcFMElectricTraceOutputs {
  */
 @activate(ChannelProvider)
 export class RunArcFMElectricTrace implements IActivityHandler {
-  async execute(
-    inputs: RunArcFMElectricTraceInputs,
-    context: IActivityContext,
-    type: typeof ChannelProvider
-  ): Promise<RunArcFMElectricTraceOutputs> {
-    const {
-      phasesToTrace = "Any",
-      serviceUrl,
-      traceType = "Downstream",
-      ...other
-    } = inputs;
-    if (!serviceUrl) {
-      throw new Error("serviceUrl");
+    async execute(
+        inputs: RunArcFMElectricTraceInputs,
+        context: IActivityContext,
+        type: typeof ChannelProvider
+    ): Promise<RunArcFMElectricTraceOutputs> {
+        const {
+            phasesToTrace = "Any",
+            serviceUrl,
+            traceType = "Downstream",
+            ...other
+        } = inputs;
+        if (!serviceUrl) {
+            throw new Error("serviceUrl");
+        }
+
+        const channel = type.create(undefined, "arcgis");
+        channel.request.url = `${inputs.serviceUrl}/exts/ArcFMMapServer/Electric%20Trace`;
+        channel.request.method = "POST";
+        channel.request.json = {
+            f: "json",
+            phasesToTrace,
+            traceType,
+            ...other,
+        };
+
+        await channel.send();
+
+        context.cancellationToken.finally(function () {
+            channel.cancel();
+        });
+
+        const results = (channel.response.payload as any)?.data?.results || [];
+
+        return {
+            results,
+        };
     }
-
-    const channel = type.create(undefined, "arcgis");
-    channel.request.url = `${inputs.serviceUrl}/exts/ArcFMMapServer/Electric%20Trace`;
-    channel.request.method = "POST";
-    channel.request.json = {
-      f: "json",
-      phasesToTrace,
-      traceType,
-      ...other,
-    };
-
-    await channel.send();
-
-    context.cancellationToken.finally(function () {
-      channel.cancel();
-    });
-
-    const results = (channel.response.payload as any)?.data?.results || [];
-
-    return {
-      results,
-    };
-  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import * as index from "./index";
 let mainResult: Promise<any> | undefined;
 
 export async function main(): Promise<any> {
-  if (mainResult) {
+    if (mainResult) {
+        return mainResult;
+    }
+
+    // If you need to do activity pack initialization logic
+    // that can happen here.
+
+    mainResult = Promise.resolve(index);
     return mainResult;
-  }
-
-  // If you need to do activity pack initialization logic
-  // that can happen here.
-
-  mainResult = Promise.resolve(index);
-  return mainResult;
 }
